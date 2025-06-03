f6e199ad988f4a166f6349504252cb012740b763
erzhena-d <dyrkheevaerzhena@gmail.com>
2025-06-03 03:47:30 +0000

    homework-12

---
+++ bash2/homework12/introduce.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name=Jennie
+age=25
+favorite_language=Python
+hobby=hiking
+dream="becoming a DevOps engineer"
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby."
+echo "As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language."
+echo "I code in $favorite_language almost every day!"
+echo "One day, I hope my love for $favorite_language will help me achieve my dream of $dream."
+echo "When I'm not busy working toward $dream, you'll find me $hobby to relax."
+echo "That's a little about me, $name — a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
