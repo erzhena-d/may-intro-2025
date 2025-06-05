915b74da4f0cc280ff7ee49d036bca16e3d21d33
erzhena-d <dyrkheevaerzhena@gmail.com>
2025-06-05 14:58:00 +0000

    homework 13, bash scripts

---
+++ bash2/homework13/age.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "Enter your year of birth: " year
+echo "You are $((2025-year)) years old"
+echo "In 2020 you were $((2020-year)) years old"
+echo "In 2030 you'll be $((2030-year)) years old"
