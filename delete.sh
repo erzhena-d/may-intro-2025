6fb5f4d8f87612727c865dbd397889d0f571b720
erzhena-d <dyrkheevaerzhena@gmail.com>
2025-06-07 21:19:54 +0000

    structured repo

--- delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo userdel -r tim
-sudo userdel -r ann
-sudo userdel -r brad
-
-sudo rm -rf file1 folder1
--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-mkdir folder1
-touch file1 
-sudo useradd tim
-sudo chown tim folder1
